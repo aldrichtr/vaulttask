416b8e4fd8058bb9129438293eac20e738d03c74
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2024-07-24 16:53:59 -0400

    wip: fixed compiler issue.  Now parsing markdown

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,14 @@ mod command;
 mod config;
 mod task;
 
+use markdown_it::{parser::inline::{InlineRoot, Text}, plugins::cmark::block::heading::ATXHeading};
+use markdown_it_front_matter::FrontMatter;
+use serde_yaml;
 
-use crate::{
-    cli::Cli,
-    config::Config,
-    task::parser::Parser
-};
+use crate::{cli::Cli, config::Config, task::parser::Parser};
 // endregion imports
 
 fn main() {
-
     // 1. determine if we have ever run before.
     //    - we never have
     //      1. Ask where the vault(s) are
@@ -27,19 +25,39 @@ fn main() {
     let mut parser = Parser::new();
 
     // region: just for debug purposes
-    println!( "Debug set to {:?}", config.debug);
+    println!("Debug set to {:?}", config.debug);
 
     println!("the path would be {:?}", config.vaults[0].to_glob());
 
-    for entry in config.vaults[0].get_files().expect("Failed to read glob pattern") {
+    for entry in config.vaults[0]
+        .get_files()
+        .expect("Failed to read glob pattern")
+    {
         match entry {
-            Ok(path) => println!("file: {:?}", path.display()),
-            Err(e) => println!("error: {:?}", e)
+            Ok(path) => {
+                println!("file: {:?}", path.display());
+                let ast = parser.parse(&path);
+                ast.walk(|node, _depth| {
+                    if let Some(fm) = node.cast::<FrontMatter>() {
+                        println!("The front matter content: {}", fm.content);
+                    }
+                    else if let Some(hd) = node.cast::<ATXHeading>() {
+                        println!("Children are: {:#?}", node.children);
+                        if let Some(h_title) = node.children[0].cast::<Text>() {
+                            let h_title = h_title.content.as_str();
+                            println!("This heading is level {} with title {}", hd.level, h_title);
+                        }
+                    } else {
+                        println!("This node is {:?} at depth {}", node.name(), _depth);
+                    }
+                });
+                // println!("md ast: {:?}", parser.parse(&path));
+            }
+            Err(e) => println!("error: {:?}", e),
         }
     }
     // endregion just for debug purposes
 
     let cli = Cli::new(config);
     cli.run();
-
 }
--- src/task/parser.rs
+++ src/task/parser.rs
@@ -1,48 +1,35 @@
 // region: imports
 //- stdlib
 use std::{
-    fs::{read_to_string, OpenOptions}, io::read_to_string, path::PathBuf
+    fs::read_to_string,
+    path::PathBuf
 };
 
-use chrono::format::parse;
 //- crates
 use markdown_it::{MarkdownIt, Node};
-use markdown_it_front_matter::FrontMatter;
-use markdown_it_tasklist::TodoCheckbox;
-use markdown_it::plugins::cmark;
 //- local
 
 // endregion imports
 
 pub struct Parser {
-    plugins: Vec<MarkdownIt>,
-    _parser: MarkdownIt
+    parser: MarkdownIt
 }
+
 
 impl Parser {
     pub fn new() -> Self {
+        let mut parser =  markdown_it::MarkdownIt::new();
+        markdown_it::plugins::cmark::add(&mut parser);
+        markdown_it_front_matter::add(&mut parser);
+        markdown_it_tasklist::add(&mut parser);
         Self {
-            plugins: vec![
-                cmark,
-                FrontMatter,
-                TodoCheckbox
-            ],
-            _parser: MarkdownIt::new()
+            parser
         }
     }
 
-    pub fn add_plugin(&self, md: &MarkdownIt) -> Self {
-        self.plugins.push(md);
-        self
-    }
-
-    pub fn parse(&self, path: & PathBuf) -> Node {
-        for plugin in self.plugins {
-            plugin::add(self._parser);
-        };
-
-        let content: String = fs::read_to_string(path.display());
-        self._parser::parse(content.as_str())
+    pub fn parse(&mut self, path: & PathBuf) -> Node {
+        let content: String = read_to_string(path).expect("Could not read markdown file");
+        self.parser.parse(content.as_str())
     }
 
 }
